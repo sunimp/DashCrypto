e7e6e1014bb9367a5cbb9944ac66fb9c398ccc5c
杨光 <yangguang@webull.com>
2024-08-14 19:20:02 +0800

    fix: bump version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "DashCrypto",
-  platforms: [.iOS(.v10)],
+  platforms: [.iOS(.v13)],
   products: [
     .library(name: "DashCrypto",
              targets: ["BlsKit", "X11Kit"])
